bb149db5f1d54389e129781dda944d188964a9ed
Will McEnaney <will.mcenaney@gmail.com>
2021-02-28 16:18:20 +0000

    Flick API test

---
+++ src/index.js
@@ -0,0 +1,23 @@
+#!/bin/env node
+
+var settings = require('settings.json');
+
+var Flickr = require('flickr-sdk'), flickrOptions = {
+    api_key: settings.consumerKey,
+    secret: settings.consumerSecret
+};
+
+Flickr.authenticate(flickrOptions, function(error, flickr){
+    flickr.people.getPhotos({
+        user_id : "me",
+        authenticated: "true"
+    }, function(err,result){
+        if(err){
+            print(err);
+            return;
+        }
+        print(result);
+    })
+})
+
+
